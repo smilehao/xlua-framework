a473d97eaec26804783eee8c95d7de0c81eaf9d0
DESKTOP-RDLLC6S\Administrator <703016035@qq.com>
2017-12-30 16:58:41 +0800

    拿掉不需要的using

--- Assets/Scripts/GameLuach/AssetbundleUpdater.cs
+++ Assets/Scripts/GameLuach/AssetbundleUpdater.cs
@@ -3,7 +3,6 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using AssetBundles;
-using UnityEngine.SceneManagement;
 
 public class AssetbundleUpdater : MonoBehaviour
 {
