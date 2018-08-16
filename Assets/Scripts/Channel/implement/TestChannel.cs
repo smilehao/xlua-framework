bba02e3498c94bf5bbc9132cb1ed2a778533ec84
smilehao <703016035@qq.com>
2018-08-16 19:46:20 +0800

    Unity2017版本以上先不走Andorid sdk，目前构建时有问题

--- Assets/Scripts/Channel/implement/TestChannel.cs
+++ Assets/Scripts/Channel/implement/TestChannel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+/// <summary>
+/// 说明：内部测试渠道
+/// 
+/// 注意：
+/// 1、Unity版本5.3.4f1，使用andorid sdk没有任何问题
+/// 2、Unity版本5.3.4f1以上，Unity2017版本以下，需要改AndroidManifest中的android:targetSdkVersion值为26
+/// 3、Unity2017以上版本，构建报错，目前还没找到解决方案
+/// 
+/// @by wsh 2018-08-16
+/// </summary>
+
 namespace GameChannel
 {
     public class TestChannel : BaseChannel
     {
         public override void Init()
         {
+#if UNITY_2017_1_OR_NEWER
+            ChannelManager.instance.OnInitSDKCompleted("No use android sdk !!!");
+#else
             AndroidSDKHelper.FuncCall("TestChannelInit");
+#endif
         }
 
         public override string GetBundleID()
@@ -28,12 +43,20 @@ namespace GameChannel
         {
             string url = paramList[0] as string;
             string saveName = paramList[1] as string;
+#if UNITY_2017_1_OR_NEWER
+            Logger.LogError("No support download game !!!");
+#else
             AndroidSDKHelper.FuncCall("DownloadGame", url, saveName);
+#endif
         }
 
         public override void InstallApk()
         {
+#if UNITY_2017_1_OR_NEWER
+            Logger.LogError("No support install game !!!");
+#else
             AndroidSDKHelper.FuncCall("InstallApk");
+#endif
         }
 
         public override void Login()
