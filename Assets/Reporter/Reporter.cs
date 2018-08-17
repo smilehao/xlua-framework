300ccd5890bbaf9257ded15c36f31d3870b30125
smilehao <703016035@qq.com>
2018-08-18 04:23:18 +0800

    Reporter在Unity2018报错问题

--- Assets/Reporter/Reporter.cs
+++ Assets/Reporter/Reporter.cs
@@ -2038,7 +2038,7 @@ public class Reporter : MonoBehaviour {
             url = System.IO.Path.Combine(streamingAssetsPath, prefFile);
         }
 
-#if !UNITY_2017
+#if !UNITY_2017_1_OR_NEWER
         if (Application.platform != RuntimePlatform.OSXWebPlayer && Application.platform != RuntimePlatform.WindowsWebPlayer)
             if (!url.Contains("://"))
                 url = "file://" + url;
